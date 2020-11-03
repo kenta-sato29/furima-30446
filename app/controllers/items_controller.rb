3bb0b7afc3af7a07c9a7cb99cfa03edd6fc57f99
kenta-sato29 <cafedeboss@outlook.jp>
2020-11-03 11:13:07 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only:[:new,:edit]
 
   def index
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+
+  end
+
 
   private
 
