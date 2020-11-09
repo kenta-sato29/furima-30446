ca725013cf27dacbb60594b2f127e9815fd13ac3
kenta-sato29 <cafedeboss@outlook.jp>
2020-11-09 18:57:27 +0900

    デプロイ修正２

--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -14,6 +14,6 @@ class UserBuy
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(post: post, shipment_source_id: shipment_source_id, first_number: first_number, second_number: second_number, building_name: building_name, purchase_id: purchase.id)
+    Address.create(post: post, tel: tel, shipment_source_id: shipment_source_id, first_number: first_number, second_number: second_number, building_name: building_name, purchase_id: purchase.id)
  end
 end
